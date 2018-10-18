f0f75a48bb9dc1c449caa4767101772a6e09e861
Tekane <phasha15@gmail.com>
2018-10-18 15:13:01 +0200

    online store first commit (Testing)

--- src/main/java/net/onlineshopping/Application.java
+++ src/main/java/net/onlineshopping/Application.java
@@ -1,9 +1,9 @@
-package com.onlineshopping;
-
+package net.onlineshopping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
+@ComponentScan({"net.onlineshopping.controller","net.onlineshopping.config"})
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/net/onlineshopping/controller/IndexController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ *
+ * @author tphasha
+ */
+@Controller
+public class IndexController {
+    
+    @RequestMapping(value = {"/","home"} , method = RequestMethod.GET)
+    public ModelAndView home(){
+        ModelAndView modelAndView =  new ModelAndView();
+        modelAndView.setViewName("home");
+        modelAndView.addObject("message","Phasha"+" ke boss");
+        return modelAndView;
+    }
+}
