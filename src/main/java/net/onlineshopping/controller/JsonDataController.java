9ede6abf1fb2ce3b3944957ec96baabb48a120ac
tekane <tekane@Kgodisho>
2018-11-20 03:29:27 +0200

    Merging brunches

---
+++ src/main/java/net/onlineshopping/controller/JsonDataController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.onlineshopping.controller;
+import java.util.List;
+import net.PTSonlineshoppingback_end.model.Product;
+import net.PTSonlineshoppingback_end.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ *
+ * @author tekane
+ */
+@RestController
+//@RequestMapping("/api")
+public class JsonDataController {
+   @Autowired
+    private  ProductService productService;
+
+    @RequestMapping("/allProducts")
+    @ResponseBody
+    public List<Product> getActiveProducts(){
+        List<Product> products =  this.productService.getActiveProducts();
+        return  products;
+    }
+    @GetMapping("/category/{id}/products")
+    @ResponseBody
+    public List<Product> getActiveProductsByCategoryId(@PathVariable("id") int id){
+        List<Product> products =  this.productService.getActiveProductsByCategorId(id);
+        return  products;
+    }
+}
